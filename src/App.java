846fadce839dcd08800403dd2b7e52bda3b28d8a
KevinEPNSCV <kevin.calles@epn.edu.ec>
2024-02-25 17:40:39 -0500

    InterfazPErsonal

--- src/App.java
+++ src/App.java
@@ -1,37 +1,37 @@
+import java.util.List;
+
+import DataAccessComponent.AnimalDAO;
+import DataAccessComponent.ClasificacionDAO;
+import DataAccessComponent.DTO.AnimalDTO;
+import UserInterface.Form.SplashScreenForm;
 import UserInterface.Form.MainForm;
 public class App {
     public static void main(String[] args) throws Exception {
+        try {
+            //Metodo para visualizar el SplashScreen
+            SplashScreenForm.show();
+            
+            // Supongamos que tienes una instancia de la clase donde se encuentra el método readAllEstructura
+            // y que su nombre es AnimalDAO. Reemplaza AnimalDAO con el nombre correcto de tu clase si es diferente.
+            AnimalDAO animalDAO = new AnimalDAO();
         
+            // Llamamos al método readAllEstructura con el tipo de hábitat "Selva"
+            List<AnimalDTO> listaAnimalesSelva = animalDAO.readAllEstructura("Seva");
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+            // Imprimimos los resultados
+            for (AnimalDTO animal : listaAnimalesSelva) {
+                System.out.println("ID: " + animal.getIdAnimal());
+                System.out.println("Sexo: " + animal.getSexo());
+                System.out.println("Habitat: " + animal.getHabitat());
+                System.out.println("Clasificacion: " + animal.getClasificacion());
+                System.out.println("Nombre: " + animal.getNombre());
+                System.out.println("QR: " + animal.getQr());
+                System.out.println("--------------------");
+            }
+        } catch (Exception e) {
+            // Manejo de excepciones
+            e.printStackTrace();
+        }
         MainForm  mf = new MainForm("APP");
     }
 }
