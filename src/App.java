d05b0e14a42c6879dfce57deb6a0116d71d6341c
KevinEPNSCV <kevin.calles@epn.edu.ec>
2024-02-25 17:36:09 -0500

    InterfazPersonal

--- src/App.java
+++ src/App.java
@@ -1,6 +1,37 @@
 import UserInterface.Form.MainForm;
 public class App {
     public static void main(String[] args) throws Exception {
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         MainForm  mf = new MainForm("APP");
     }
 }
